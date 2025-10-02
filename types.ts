569014432a03c2fa398bc1d4f606439c91d2e3de
taher-design <taher4youus@yahoo.com>
2025-10-03 01:42:39 +0300

    Create types.ts

---
+++ types.ts
@@ -0,0 +1,18 @@
+
+export interface DomainSuggestion {
+  domainName: string;
+  justification: string;
+  estimatedValue: number;
+  slogan: string;
+}
+
+export interface Resource {
+  name: string;
+  url: string;
+  description: string;
+}
+
+export interface Niche {
+  niche: string;
+  description: string;
+}
